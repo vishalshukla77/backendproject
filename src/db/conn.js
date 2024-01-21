4a0a447e9db8bdeac742d1b1f6d9774bbf394d74
vishalshukla77 <vishalshukla1131@gmail.com>
2024-01-21 13:24:08 +0530

    updation

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/registration").then(() => {
+const mongo_URI="mongodb://127.0.0.1:27017/registration";
+mongoose.connect(mongo_URI).then(() => {
   console.log(`Connection successful`);
 }).catch((e) => {
   console.log(`No connection`);
