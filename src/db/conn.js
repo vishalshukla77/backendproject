ecfae802abd93e6b66109b6eb68d4a412b5fdfb5
vishalshukla77 <vishalshukla1131@gmail.com>
2024-01-21 15:05:30 +0530

    updation

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose");
-const mongo_URI="mongodb://127.0.0.1:27017/registration";
-mongoose.connect(mongo_URI).then(() => {
-  console.log(`Connection successful`);
-}).catch((e) => {
-  console.log(`No connection`);
-});
+
+
+const defaultDatabaseName = "registration";
+
+
+const dbName = process.argv[2] || defaultDatabaseName;
+
+const mongo_URI = `mongodb://127.0.0.1:27017/${dbName}`;
+
+mongoose.connect(mongo_URI)
+  .then(() => {
+    console.log(`Connection successful to database: ${dbName}`);
+  })
+  .catch((e) => {
+    console.log(`No connection to database: ${dbName}`);
+  });
